d898ddeabb617abbc1d562351ce6118e3f97bd0a
davchile <davchile@hotmail.com>
2026-05-04 21:12:24 -0400

    we got the comments part

--- python-test.py
+++ python-test.py
@@ -1,4 +1,5 @@
-print("tengo sueño")
-print("second line")
-print("tercera linea")
-print("cuarta linea")
+name = input("What's you name? ") # name is a variable and input is a function
+"""
+This is a comment
+"""
+print("hello", name, ", how are you?") # estoy añadiendo la variable al string. Dos opciones para agregar variables. El signo + y la coma.
