d4838347e6e6b02bfaad03ba7f289bd3d1e69c3a
davchile <davchile@hotmail.com>
2026-05-13 23:11:27 -0400

    using main in calculator.py

--- calculator.py
+++ calculator.py
@@ -13,7 +13,7 @@ y = int(input("What's y? "))
 
 print(x + y)
 """
-
+"""
 # float → another type of data (number with a decimal) with function round included. It shows result always with decimals and commas as separator. Conditionals are needed if we want to show int numbers without decimals.
 x = float(input("What's x? "))
 y = float(input("What's y? "))
@@ -25,13 +25,26 @@ y = float(input("What's y? "))
 # print(f"{z:,}") # put a comma to separate digits
 
 # division and round with two digits
-"""z = round(x / y, 2)
+z = round(x / y, 2)
 
 print(z)
-"""
+
 # but if I want to use a format string. Here I am separating digits and having two decimals using format string.
 z = x / y
 
 print(f"{z:,.2f}")
+"""
+# getting a return value and using main()
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
 
+def square(n): #generically n, this can be anything I want
+    #return n * n
 
+# another way to solve the math
+    #return n ** 2 # the two asterisks raises "n" to the power of "2"
+
+# and another way to solve the math
+    return pow(n, 2) # this function has to arguments: the number and the exponent
+main()
