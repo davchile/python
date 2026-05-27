9498d422ca2c27aa26604a1d3f2bec974296af1b
davchile <davchile@hotmail.com>
2026-05-26 22:08:29 -0400

    using match and single bar

---
+++ house.py
@@ -0,0 +1,45 @@
+# Basic way of doing questions
+name = input("What's you name? ")
+"""
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who")
+"""
+# Now we can tighten this code
+"""
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who")
+"""
+# now having a basic way of using "match"
+"""
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _: # similarly to "else" construct this underline means to say "whatever case has not yet been handled" go ahead and, in this case print "Who"
+        print("Who")
+"""
+# and finally using a single bar to express the same idea bot more concisely
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who")
---
+++ parity.py
@@ -0,0 +1,28 @@
+"""
+x = int(input("What's x? "))
+ 
+if x % 2 == 0:
+    print("Even")
+ 
+else:
+    print("Odd")
+"""
+# Using a main function
+def main():
+    x = int(input("what's x? ")) # I'm using is_even here to make a decision, in this case by printing "Even" or "Odd".
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+"""        
+def is_even(n): # now that I have my own boolean function that answers this question for me, I can use it in this program forward o another one.
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+"""
+# this is the most succint and elegant way of using conditional in Python
+def is_even(n):
+    return n % 2 == 0
+
+main()
